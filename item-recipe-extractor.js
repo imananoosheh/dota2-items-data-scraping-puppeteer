b1207e11ce73113026a59a67caf3d2f8b340a424
Iman Anooshehpour <imananoosheh@gmail.com>
2023-05-03 12:53:14 -0700

    item recipe first phase os extraction is done.

--- item-recipe-extractor.js
+++ item-recipe-extractor.js
@@ -1,14 +1,37 @@
-//document.querySelectorAll('tbody tbody tr:last-child')[0].querySelectorAll('a')
-// [1:length]
-let ingredients = Array.from(document.querySelectorAll('tbody tbody tr:last-child')[0].querySelectorAll('a'), (aTag,k)=>{
-    return {
-        name: aTag.getAttribute('title').split('(')[0].trim(),
-        itemURL: aTag.getAttribute('href').trim(),
-        thumbnailURL: aTag.querySelector('img').src.split('scale-to-width-down')[0],
-    }
-})
-let itemMadeOf = ingredients.splice(0,1)
-let record = {}
-record[itemMadeOf[0].name] = ingredients
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const dotaItemData = require('./dota-item-cleaned-phase1.json')
 
-console.log(record)
+async function getRecipes(){
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    for(let i=0; i<11; i++){
+        let currentSubCat = dotaItemData[i]
+        for(let j=0; j< currentSubCat.length; j++){
+            const currentItemURL = currentSubCat[j]['itemURL']
+            console.log('entring this url:',currentItemURL)
+            await page.goto(currentItemURL);
+
+            let ingredients = await page.evaluate(()=> {
+                let currentAnchors = Array.from(document.querySelectorAll('tbody tbody tr:last-child')[0].querySelectorAll('a'), (aTag,k)=>{
+                    console.log(aTag)
+                    return {
+                        name: aTag.getAttribute('title').split('(')[0].trim(),
+                        itemURL: aTag.getAttribute('href').trim(),
+                        thumbnailURL: aTag.querySelector('img').src.split('scale-to-width-down')[0],
+                    }
+                })
+                console.log(currentAnchors)
+                let itemMadeOf = currentAnchors.splice(0,1)
+                let record = {}
+                record[itemMadeOf[0].name] = currentAnchors
+                return record
+            })
+            console.log(ingredients)
+        }
+    }
+}
+getRecipes()
+
+//TODO: Add Differentiator for "UsedOf" and "Components" from alt text of img between anchor tags
